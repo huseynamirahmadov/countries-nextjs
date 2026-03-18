68bf9b8d6c41523674778c13bb0808eb1dfcad0e
unknown <huseynamirahmadov@gmail.com>
2026-03-18 14:52:48 +0400

    build13

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Sort from './Sort'
 const Header = async () => {
 
   const fetchCountries = async () => {
-    const res = await fetch('https://restcountries.com/v3.1/all', { cache: 'no-store' })
+    const res = await fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags,region,unMember,cca3,borders,population,maps', { cache: 'no-store' })
     return res.json()
   }
   const data = await fetchCountries()
